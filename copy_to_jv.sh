dfd05142351378d0ef95adda1b3c6463f815b625
Samuel Carbone <scar7732@uni.sydney.edu.au>
2020-09-30 15:12:51 +0800

    Added an initial wait in the copy_to_jv script

--- copy_to_jv.sh
+++ copy_to_jv.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 jevois-usbsd start && \
+sleep 1 && \
 cp ./pbuild/Main /media/samuel/JEVOIS/scripts && \
 cp ./launch-platform.sh /media/samuel/JEVOIS/scripts && \
 sleep 5 && \
