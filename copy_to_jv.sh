fde26d5733507589ba40cfb961af1a8d45bea34d
Samuel Carbone <scar7732@uni.sydney.edu.au>
2020-10-06 15:08:23 +0800

    Turn off stream before mounting

--- copy_to_jv.sh
+++ copy_to_jv.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+echo "streamoff" > /dev/ttyACM0 && \
+sleep 1 && \
 jevois-usbsd start && \
 sleep 1 && \
 cp ./pbuild/Main /media/samuel/JEVOIS/scripts && \
 cp ./launch-platform.sh /media/samuel/JEVOIS/scripts && \
-sleep 5 && \
+sleep 1 && \
 jevois-usbsd stop && \
 echo "Successful copy"
 
