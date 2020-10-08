ca04ff713766728dd4f22a85ca557e1b88813262
Samuel Carbone <scar7732@uni.sydney.edu.au>
2020-10-08 17:58:50 +0800

    Clear serial before copy

--- copy_to_jv.sh
+++ copy_to_jv.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 echo "streamoff" > /dev/ttyACM0 && \
-sleep 1 && \
+sleep 1
+
+# First read any accumulated junk in the serial buffers:
+while true; do
+sudo bash -c "read -s -t 0.05 -s < \"/dev/ttyACM0\""
+if [ $? -ne 0 ]; then break; fi
+done
+
+sleep 1
+
 jevois-usbsd start && \
 sleep 1 && \
 cp ./pbuild/Main /media/samuel/JEVOIS/scripts && \
